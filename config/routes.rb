c8799693f83c617becb5c01a81ce17dd6a57c11e
nghiadk <nghiadk1102@gmail.com>
2017-07-11 13:48:55 +0700

    Add login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    confirmations: "users/confirmations"
+  }
+
+  devise_scope :user do
+    get "/users/sign_out" => "devise/sessions#destroy"
+  end
   root "static_pages#home"
   get "/help", to: "static_pages#help"
 end
